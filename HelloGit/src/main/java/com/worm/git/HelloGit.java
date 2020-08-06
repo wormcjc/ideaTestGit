7a0c77772563b0468586dfd59bd162e21b11a0f3
cjc <1142282541@qq.com>
2020-08-06 10:52:06 +0800

    创建分支

--- HelloGit/src/main/java/com/worm/git/HelloGit.java
+++ HelloGit/src/main/java/com/worm/git/HelloGit.java
@@ -9,5 +9,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello GIT!");
         System.out.println("更新！");
+        System.out.println("创建分支");
     }
 }
