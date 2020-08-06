bd3aab5b28c586c33c5746b27fdeb1d3d2224da4
cjc <1142282541@qq.com>
2020-08-06 09:18:42 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/worm/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.worm.git;
+
+/**
+ * @Author Worm
+ * @Date 2020/8/6 8:52
+ * @Version 1.0
+ **/
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello GIT!");
+    }
+}
